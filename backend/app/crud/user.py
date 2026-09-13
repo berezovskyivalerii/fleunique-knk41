b5cfd38bc689a52f0aad00d95bf3fbcbf8321b3b
tREON222 <1967havana@gmail.com>
2026-09-13 05:10:54 +0300

    feat: implement admin seeding

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -20,3 +20,23 @@ def create_user(db: Session, user: UserCreate, hashed_password: str) -> User:
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def create_admin_user(
+    db: Session,
+    full_name: str,
+    email: str,
+    phone_number: str | None,
+    hashed_password: str,
+) -> User:
+    db_user = User(
+        full_name=full_name,
+        email=email,
+        phone_number=phone_number,
+        hashed_password=hashed_password,
+        is_admin=True,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
